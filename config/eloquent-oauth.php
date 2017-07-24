cc2601ae4bc7c6b5b6940e30c35127c2367a79ca
dika <aditiaaadicka@yahoo.com>
2017-07-24 11:30:16 +0700

    laravel-socialmedia

---
+++ config/eloquent-oauth.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\User;
+
+return [
+    'model' => User::class,
+    'table' => 'oauth_identities',
+    'providers' => [
+        'facebook' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/facebook/redirect',
+            'scope' => [],
+        ],
+        'google' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/google/redirect',
+            'scope' => [],
+        ],
+        'github' => [
+            'client_id' => '718054bff555383b7de7',
+            'client_secret' => 'b6f9b0c1d4ee3bb17de51d932d31dc07852b964e',
+            'redirect_uri' => 'http://socialmedia.dev/github/redirect',
+            'scope' => [],
+        ],
+        'linkedin' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/linkedin/redirect',
+            'scope' => [],
+        ],
+        'instagram' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/instagram/redirect',
+            'scope' => [],
+        ],
+        'soundcloud' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/soundcloud/redirect',
+            'scope' => [],
+        ],
+    ],
+];
